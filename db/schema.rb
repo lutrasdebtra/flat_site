1ba2d633ecca9f8dd0be788b7bf2ffc5e60e4926
lutrasdebtra <stuy.bradley@gmail.com>
2015-02-26 12:44:46 +1300

    Add user-payment relation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225233342) do
+ActiveRecord::Schema.define(version: 20150225234349) do
 
   create_table "payments", force: :cascade do |t|
     t.date     "date"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150225233342) do
     t.decimal  "paykn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
